3e0e379fb8fe3033be0f7ab6c5085332c4f0ea56
Farouk1995Specialiste <wardifarouk22@gmail.com>
2024-05-17 12:56:56 +0100

    i love js

--- improveScript.js
+++ improveScript.js
@@ -11,33 +11,44 @@ const size = document.getElementById('size');
 // variables
 let square;
 const DEFAULT_SIZE=32;
+const DEFAULT_MODE='black'
 
 let currentSize=DEFAULT_SIZE;
+let currentMode=DEFAULT_MODE;
 let rainbow_mode = false ;
 let black_mode = false;
 let eraser_mode = false;
 let clear_mode= false;
+
 // SET CURRENT SIZE
 function setCurrentSize(newSize){
     currentSize=newSize;
 }
-
-
-sizeSlider.onchange = (e) => {
-    changeSize(e.target.value)
-   // console.log(e.target.value)
+function setcurrentMode (newMode){
+currentMode =newMode
 }
-clearbtn.addEventListener('click',reloadGrid)
-clearbtn.addEventListener('click',(e)=>toglleMode(e.target))
+let mousedown=false;
+document.body.onmousedown = () =>(mousedown=true);
+document.body.onmouseup = () => (mousedown =false);
 
-eraserBtn.addEventListener('click',()=> eraseSquare(square));
-eraserBtn.addEventListener('click',(e)=>toglleMode(e.target));
+sizeSlider.onchange = (e) =>changeSize(e.target.value)
+ rainbowBtn.onclick =(e) =>{
+     toglleMode(e.target)
+    setcurrentMode('random')}  
+ blackBtn.onclick = (e) => {
+    setcurrentMode('black');
+    toglleMode(e.target)
+ }
 
-blackBtn.addEventListener('click',()=> blackColor(square));
-blackBtn.addEventListener('click',(e)=>toglleMode(e.target))
+clearbtn.addEventListener('click',(e)=>{
+    reloadGrid(e);
+    toglleMode(e.target);
+})
+eraserBtn.addEventListener('click',(e)=>{
+      setcurrentMode('eraser')
+     toglleMode(e.target)
+    });
 
-rainbowBtn.addEventListener('click',(e)=>{toglleMode(e.target)});
-rainbowBtn.addEventListener('click',()=>randomColor(square));
 
 function toglleMode (node){
    const modeButtons = [rainbowBtn ,blackBtn ,eraserBtn,clearbtn]
@@ -113,12 +124,27 @@ function createGrid(size){
             let column = document.createElement('div');
             column.classList.add('square')
             row.appendChild(column);
+            column.addEventListener('mousedown',changeColor);
+            column.addEventListener('mouseover',changeColor)
             square=document.querySelectorAll('.square');
          //   console.log(square)
         }  
         
        }
 }
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mousedown) return
+    if (currentMode === 'random') {
+      const randomR = Math.floor(Math.random() * 256)
+      const randomG = Math.floor(Math.random() * 256)
+      const randomB = Math.floor(Math.random() * 256)
+      e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
+    } else if (currentMode === 'black') {
+      e.target.style.backgroundColor = 'black'
+    } else if(currentMode === 'eraser'){
+     e.target.style.backgroundColor=`#dde3e6`
+    }
+  }
 //Update Size Value 
 function updateSizeValue(value) {
   size.innerHTML=`${value} x ${value}`
@@ -128,44 +154,10 @@ function updateSizeValue(value) {
 function clearAll(){
     grid.innerHTML=``;
     }
-// 
-
-// Random Color
-function randomColor(square){
-   
-    return (
-     square.forEach((q)=>q.addEventListener('mouseover',(e)=>{
-     const red = Math.round(Math.random() * 255);
-     const blue = Math.round(Math.random() * 255);
-     const green = Math.round(Math.random() * 255);
- e.target.style.backgroundColor=`rgb(${red},${green},${blue})`
-    
-     }))   
- ) 
- 
- }
-
- // black color
- function blackColor(square){
-    return (
-        square.forEach(s=>s.addEventListener('mouseover',(e)=>{
-            e.target.style.backgroundColor='black'
-        }))
-    )
-}
-
-
-function eraseSquare(square){
-    return (
-        square.forEach(s=>s.addEventListener('mouseover',(e)=>{
-            e.target.style.backgroundColor= `#dde3e6`
-        }))
-    )
-}
-
 window.onload =() =>{
     createGrid(DEFAULT_SIZE);
     updateSizeValue(DEFAULT_SIZE)
+    toglleMode(clearbtn)
 }
   
 
